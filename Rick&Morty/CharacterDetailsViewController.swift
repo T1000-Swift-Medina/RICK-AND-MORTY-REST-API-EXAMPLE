0336ef76ffcf59d05029e2f889760f795894c7ea
Abdulaziz Alfayaa <abdulaziz.alfayaa@tuwaiq.edu.sa>
2021-11-14 02:59:40 +0300

    Add UINavigationController to initialed view controller Add CharacterDetailsViewController to show Character Details

---
+++ Rick&Morty/CharacterDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterDetailsViewController.swift
+//  Rick&Morty
+//
+//  Created by Abdulaziz Alfayaa on 14/11/2021.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
